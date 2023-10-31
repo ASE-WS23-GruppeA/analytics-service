74954cd6058ed90ec9200fd14f3d996c23bbf8c4
Rafael <rafael.agashirinov@gmail.com>
2023-10-31 10:45:14 +0100

    New changes in the code

---
+++ src/test/java/com/WorkoutTrackerAnalyticsServiceTest/WorkoutTrackerAnalyticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WorkoutTrackerAnalyticsServiceTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutTrackerAnalyticsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
