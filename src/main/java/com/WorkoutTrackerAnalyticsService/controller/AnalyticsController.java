5be285ae9f1b615c42b172a05010529ab3236c3d
Rafael <rafael.agashirinov@gmail.com>
2024-01-25 07:34:48 +0100

    Fixed some errors

--- src/main/java/com/WorkoutTrackerAnalyticsService/controller/AnalyticsController.java
+++ src/main/java/com/WorkoutTrackerAnalyticsService/controller/AnalyticsController.java
@@ -8,11 +8,15 @@ import com.WorkoutTrackerAnalyticsService.service.impl.AnalyticsServiceImpl;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+
+
 
 
 @RestController
